d8b7bf63731f0eba65f681ca3288141c8d8e0982
Jake <54716860+JakevdKolk@users.noreply.github.com>
2025-10-30 10:25:21 +0100

    added shooting with space bar

---
+++ new-game-project/Scenes/bullet.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+var speed = 600
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
--- new-game-project/Scripts/player.gd
+++ new-game-project/Scripts/player.gd
@@ -2,12 +2,21 @@ extends CharacterBody2D
 
 var speed = 300
 @onready var _animated_sprite = $Sprite
-
-
+@export var bullet_scene: PackedScene
+@onready var muzzle: Marker2D = $MuzzleShoot            
+	
 func _process(delta: float) -> void:
 	_animated_sprite.play("default")
 	look_at(get_global_mouse_position())
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
+func shoot():
+	var b := bullet_scene.instantiate()
+	get_parent().add_child(b)                 
+	b.global_transform = muzzle.global_transform
+	
+	
 func _physics_process(delta: float) -> void:
 	
 	var move_dir = Vector2(Input.get_axis("move_left" , "move_right"),
